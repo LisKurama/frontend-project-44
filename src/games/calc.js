9036dca195f992c96de9c82b48cb017824a88587
Alina Vasfieva <l.kurama@yandex.ru>
2024-04-23 15:12:50 +0400

    add

---
+++ src/games/calc.js
@@ -0,0 +1,44 @@
+import {
+  cons, car, cdr,
+} from '@hexlet/pairs';
+import game from '../index.js';
+
+const description = 'What is the result of the expression?';
+
+const operator = ['+', '-', '*'];
+
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const pair = cons(getRandomNumber(0, 10), getRandomNumber(0, 10));
+const a = car(pair);
+const b = cdr(pair);
+
+const random = Math.floor(Math.random() * operator.length);
+
+function getQuestionAndAnswer() {
+  const question = `${a} ${operator[random]} ${b}`;
+
+  let correctAnswer;
+
+  switch (operator[random]) {
+    case '+':
+      correctAnswer = a + b;
+      break;
+    case '-':
+      correctAnswer = a - b;
+      break;
+    case '*':
+      correctAnswer = a * b;
+      break;
+    default:
+      correctAnswer = NaN;
+  }
+
+  return [question, correctAnswer.toString()];
+}
+
+export default () => {
+  game(description, getQuestionAndAnswer);
+};
