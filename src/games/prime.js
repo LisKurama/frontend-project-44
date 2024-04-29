162f8ac86d6ea7edd1df051eb15fadfca6512d46
Alina Vasfieva <l.kurama@yandex.ru>
2024-04-29 10:24:08 +0400

    update src/games/prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -9,16 +9,17 @@ function getRandomNumber(min, max) {
 function getQuestionAndAnswer() {
   const question = getRandomNumber(2, 30);
 
-  let correctAnswer;
-
-  for (let i = 1; i < question; i += 1) {
-    if (question % i === 0 && i !== 1) {
-      correctAnswer = 'no';
-      break;
-    } else {
-      correctAnswer = 'yes';
+  const isPrime = (number) => {
+    for (let i = 1; i < number; i += 1) {
+      if (number % i === 0 && i !== 1) {
+        return false;
+      }
     }
-  }
+    return true;
+  };
+
+  const correctAnswer = isPrime(question) ? 'yes' : 'no';
+
   return [question.toString(), correctAnswer];
 }
 
