ef82ec7828e36ae254a6ea2f0a346e8d5f8736ac
Alina Vasfieva <l.kurama@yandex.ru>
2024-04-30 21:17:07 +0400

    update src/games/calc.js

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,5 +1,5 @@
 import game from '../index.js';
-import { getRandomNumber } from './utils.js';
+import getRandomNumber from './utils.js';
 
 const description = 'What is the result of the expression?';
 
