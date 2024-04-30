4ca40e7f73b02e2452071b3c1fafbc471a3215f3
Alina Vasfieva <l.kurama@yandex.ru>
2024-04-30 11:12:11 +0400

    update src/games/progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,21 +1,21 @@
 import game from '../index.js';
+import { getRandomNumber, getRandom } from './utils.js';
 
 const description = 'What number is missing in the progression?';
 
-function getRandomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
 function getQuestionAndAnswer() {
+  const length = getRandomNumber(5, 8);
+  const start = getRandomNumber(1, 10);
   const increment = getRandomNumber(2, 10);
 
   const result = [];
   const replacingElement = '..';
 
-  for (let i = 1; i <= 50; i += increment) {
-    result.push(i);
+  for (let i = 0; i <= length; i += 1) {
+    result.push(start + i * increment);
   }
-  const a = Math.floor(Math.random() * result.length);
+
+  const a = getRandom(result);
   result[a] = replacingElement;
 
   const string = result.join(' ');
@@ -24,10 +24,10 @@ function getQuestionAndAnswer() {
   let correctAnswer;
 
   for (let j = 1; j < result.length; j += 1) {
-    if (result[a] === replacingElement && result[a] !== result[0]) {
+    if (result[a] === replacingElement && result[0] !== replacingElement) {
       correctAnswer = result[a - 1] + increment;
     } else {
-      correctAnswer = 1;
+      correctAnswer = result[1] - increment;
     }
   }
 
