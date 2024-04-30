136eb66d6e6609f152ec83d331e239e2a059784f
Alina Vasfieva <l.kurama@yandex.ru>
2024-04-30 13:59:34 +0400

    update src/games/utils.js

--- src/games/utils.js
+++ src/games/utils.js
@@ -1,5 +1,9 @@
-const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+export const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
-const getRandom = (result) => Math.floor(Math.random() * result.length);
-
-export { getRandomNumber, getRandom };
+export const generateProgression = (increment, start, length) => {
+  const result = [];
+  for (let i = 0; i <= length; i += 1) {
+    result.push(start + i * increment);
+  }
+  return result;
+};
